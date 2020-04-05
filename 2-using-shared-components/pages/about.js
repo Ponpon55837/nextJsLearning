ab8db705b2acf707f133718d200fc394b20605f0
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-04-05 18:06:28 +0800

    upload file first

---
+++ 2-using-shared-components/pages/about.js
@@ -0,0 +1,9 @@
+import Layout from './component/Mylayout'
+
+const Page = () => {
+  return (
+    <p>This is the about page</p>
+  )
+}
+
+export default Layout(Page)
---
+++ 2-using-shared-components/pages/index.js
@@ -0,0 +1,12 @@
+import Layout from './component/Mylayout'
+
+const Page = () => {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+      <p>Tset</p>
+    </div>
+  )
+}
+
+export default Layout(Page)
