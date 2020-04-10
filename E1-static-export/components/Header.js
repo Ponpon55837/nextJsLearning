1106010829f20b14e75f72ad849970c9147e22a6
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-04-10 15:27:24 +0800

    update file setting

--- E1-static-export/components/Header.js
+++ E1-static-export/components/Header.js
@@ -5,6 +5,7 @@ const linkStyle = {
 }
 
 export default function Header() {
+  // 新增頁面的路徑連結，只要頁面是從根路徑來的
   return (
     <div>
       <Link href="/">
@@ -12,6 +13,12 @@ export default function Header() {
       </Link>
       <Link href="/about">
         <a style={linkStyle}>About</a>
+      </Link>
+      <Link href="/batman">
+        <a style={linkStyle}>Batman</a>
+      </Link>
+      <Link href="/contact">
+        <a style={linkStyle}>Contact</a>
       </Link>
     </div>
   )
