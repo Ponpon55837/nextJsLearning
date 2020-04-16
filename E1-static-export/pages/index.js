18560ef7dd9ca5091eb0086b4f194ea000668a81
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-04-16 10:53:44 +0800

    add E1 index page use api function comment

--- E1-static-export/pages/index.js
+++ E1-static-export/pages/index.js
@@ -1,22 +1,6 @@
 import Layout from '../components/MyLayout'
 import Link from 'next/link'
 
-const getPosts = () => {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ]
-}
-
-const PostLink = ({ post }) => (
-  <li>
-    <Link href="/batman/[id]" as={`/batman/${post.id}`}>
-      <a>{post.title}</a>
-    </Link>
-  </li>
-)
-
 const Index = () => {
   return (
     <Layout>
@@ -29,5 +13,23 @@ const Index = () => {
     </Layout>
   )
 }
+
+// 使用function作爲api承接內容，在上方的return中map接到的值
+const getPosts = () => {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ]
+}
+
+// 當作額外插入的link來使用，這裡面的post.id，post.title都是藉由被map進來的post值使用
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/batman/[id]" as={`/batman/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+  </li>
+)
 
 export default Index
