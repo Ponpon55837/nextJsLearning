0835bb199800f6e50b15a3f939ad101df688a39e
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-04-20 15:09:05 +0800

    add react-bootstrap setting header with responsive and toggle button

--- E1-static-export/components/Header.js
+++ E1-static-export/components/Header.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const mainStyle = {
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -11,15 +13,11 @@ const mainStyle = {
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light" style={mainStyle}>
-      <Link href="/">
-        <a className="navbar-brand">NextJsPublic</a>
-      </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
+    <Navbar collapseOnSelect expand="lg" style={mainStyle}>
+      <Navbar.Brand href="#home">NextJS with Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
           <Link href="/">
             <a className="nav-item nav-link ">Home</a>
           </Link>
@@ -35,9 +33,9 @@ const Header = () => {
           <Link href="/login">
             <a className="nav-item nav-link ">Login</a>
           </Link>
-        </div>
-      </div>
-    </nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
--- public/_next/static/runtime/webpack.js
+++ public/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "18a0f655716f8faa2433";
+/******/ 	var hotCurrentHash = "6ee007104187d8edab54";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
