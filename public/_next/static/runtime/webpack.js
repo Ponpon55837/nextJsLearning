ee4246854cbfaf200ebe85daa4a88b1fb09a6260
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-06-24 08:41:07 +0800

    update index page css setting

--- public/_next/static/runtime/webpack.js
+++ public/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4103d90e2bf914435db2";
+/******/ 	var hotCurrentHash = "b3a02b819a6052506d3a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
